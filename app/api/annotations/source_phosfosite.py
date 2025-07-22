cbec2bd354abdbc20ef1ad86a8f731c1b3763038
Fabián Robledo <fabianry97@gmail.com>
2025-07-22 09:27:23 +0200

    Source phosphosite no gives an error when the id is not found in DB

--- app/api/annotations/source_phosfosite.py
+++ app/api/annotations/source_phosfosite.py
@@ -15,12 +15,15 @@ def _load_phosphosite_from_DB(id) -> PhosphositeEntity:
     data = PhosphositeEntity.objects.filter(proteinID=id)
     return None if not data else data
     
-def _save_phosposite_in_DB(id, info):
+#def _save_phosposite_in_DB(id, info):
     # TO IMPLEMENT as there is no original ruby implementation on this
-    pass
+    # (No upstream database)
+    # I'll leave it here if this may change in the future
+    # So there is no source of data to parse
+#    pass
 
 def get_phosphositeFromUniprot(request, ensemblid):
     results = _load_phosposite_from_DB(ensemblid)
     if results is None:
-        results = []
+        results = [{"error": f"{ensemblid} not found in db"}]
     return HttpResponse(json.dumps(results),content_type='application/json')
